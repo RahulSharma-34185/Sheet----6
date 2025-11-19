c3dd83627e1d643da4211369e1c4c32bfaf1d755
Rahul Sharma <sharmar34158@gmail.com>
2025-11-19 21:42:54 +0530

    update

--- SHEET 6/StringOperations.py
+++ SHEET 6/StringOperations.py
@@ -4,14 +4,10 @@ A = A + A
 B = ""
 result = ""
 for i in range(len(A)):
-    if A[i] >= 'A' and A[i] <= 'Z':
-        continue
-    else:
-        B += A[i]
+    if A[i] >= 'A' and A[i] <= 'Z':continue
+    else:B += A[i]
 for i in range(len(B)):
-    if B[i] == 'a' or B[i] == 'e' or B[i] == 'i' or B[i] == 'o' or B[i] == 'u':
-        result += '#'
-    else:
-        result += B[i]
+    if B[i] == 'a' or B[i] == 'e' or B[i] == 'i' or B[i] == 'o' or B[i] == 'u':result += '#'
+    else:result += B[i]
 print(result)    # =>###z###z
  
